0d0d3de74b68bee454df4f2445cbef2a0db85900
Warakorn Meakpatanapinyo <warakornchamp@gmail.com>
2019-04-15 15:22:39 +0700

    implement search

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,11 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +20,18 @@ public class SimpleMovieSearchService implements MovieSearchService {
 
 	@Override
 	public List<Movie> search(String queryText) {
-		//TODO: Step 2 => Implement this method by using data from MovieDataService
-		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
-		// Please do not change @Component annotation on this class
-		return null;
+		MoviesResponse movies_list = movieDataService.fetchAll();
+		List<Movie> search_result = new ArrayList<>();
+		if(!queryText.contains(" ")) {
+			Pattern p = Pattern.compile("\\b" + queryText.toLowerCase() + "\\b");
+			for (MovieData movie : movies_list) {
+				if (p.matcher(movie.getTitle().toLowerCase()).find()) {
+					Movie tmp = new Movie(movie.getTitle());
+					tmp.getActors().addAll(movie.getCast());
+					search_result.add(tmp);
+				}
+			}
+		}
+		return search_result;
 	}
 }
