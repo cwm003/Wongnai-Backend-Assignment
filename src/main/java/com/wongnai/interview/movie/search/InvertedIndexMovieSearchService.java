3946f07b0baf4bd2cc708734def1017799c9c829
Warakorn Meakpatanapinyo <warakornchamp@gmail.com>
2019-04-17 15:54:33 +0700

    create invert index table

--- src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
@@ -1,6 +1,9 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
@@ -16,24 +19,27 @@ public class InvertedIndexMovieSearchService implements MovieSearchService {
 	@Autowired
 	private MovieRepository movieRepository;
 
+	private Map<String, List<Long>> invertedIndexTable;
+
+	public InvertedIndexMovieSearchService(){
+		invertedIndexTable = new TreeMap<>();
+		for(Movie movie:movieRepository.findAll()){
+			for(String word : movie.getName().split(" ")){
+				String wordLowerCase = word.toLowerCase();
+				Long id = movie.getId();
+				if(invertedIndexTable.containsKey(wordLowerCase)){
+					invertedIndexTable.get(wordLowerCase).add(id);
+				}else{
+					List<Long> id_list = new ArrayList<>();
+					id_list.add(id);
+					invertedIndexTable.put(wordLowerCase,id_list);
+				}
+			}
+		}
+	}
+
 	@Override
 	public List<Movie> search(String queryText) {
-		//TODO: Step 4 => Please implement in-memory inverted index to search movie by keyword.
-		// You must find a way to build inverted index before you do an actual search.
-		// Inverted index would looks like this:
-		// -------------------------------
-		// |  Term      | Movie Ids      |
-		// -------------------------------
-		// |  Star      |  5, 8, 1       |
-		// |  War       |  5, 2          |
-		// |  Trek      |  1, 8          |
-		// -------------------------------
-		// When you search with keyword "Star", you will know immediately, by looking at Term column, and see that
-		// there are 3 movie ids contains this word -- 1,5,8. Then, you can use these ids to find full movie object from repository.
-		// Another case is when you find with keyword "Star War", there are 2 terms, Star and War, then you lookup
-		// from inverted index for Star and for War so that you get movie ids 1,5,8 for Star and 2,5 for War. The result that
-		// you have to return can be union or intersection of those 2 sets of ids.
-		// By the way, in this assignment, you must use intersection so that it left for just movie id 5.
 
 		return null;
 	}
